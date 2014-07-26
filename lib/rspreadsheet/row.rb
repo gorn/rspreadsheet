20634c355f824f6fbb79cccf5ec648af5713da24
Jakub Tesinsky <jAkub.cz>
2014-07-26 02:52:51 +0200

    1-based syntax completed

--- lib/rspreadsheet/row.rb
+++ lib/rspreadsheet/row.rb
@@ -8,22 +8,9 @@ class Row
   def initialize(workbook,rowi)
     @rowi = rowi
     @workbook = workbook
-    @rowcells = RowCells.new(workbook,rowi)
   end
-  def cells
-    @rowcells
-  end
-end
-
-# this allows the row.cells[c] syntax
-# this object is result of row.cells
-class RowCells
-  def initialize(workbook,rowi)
-    @rowi = rowi
-    @workbook = workbook
-  end
-  def [] coli
-    @workbook.cells[@rowi,coli]
+  def cells(coli)
+    @workbook.cells(@rowi,coli)
   end
 end
 
