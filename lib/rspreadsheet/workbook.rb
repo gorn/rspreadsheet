22adcac0bd61a60c24ad671ad068e67115baf0a2
Jakub Těšínský <gorn@silverstone>
2016-04-04 23:59:48 +0200

    working on IO functionality tests

--- lib/rspreadsheet/workbook.rb
+++ lib/rspreadsheet/workbook.rb
@@ -66,16 +66,17 @@ class Workbook
   # Saves the worksheet. Optionally you can provide new filename.
 
   def save(new_filename_or_io_object=nil)
-    if @filename.nil? and new_filename_or_io_object.nil? then raise 'New file should be named on first save.' end
+    @par = new_filename_or_io_object
+    if @filename.nil? and @par.nil? then raise 'New file should be named on first save.' end
     
-    if new_filename_or_io_object.kind_of? StringIO
-      new_filename_or_io_object.write(@content_xml.to_s(indent: false))
-    elsif new_filename_or_io_object.nil? or new_filename_or_io_object.kind_of? String
+    if @par.kind_of? StringIO
+      @par.write(@content_xml.to_s(indent: false))
+    elsif @par.nil? or @par.kind_of? String
      
-      if new_filename_or_io_object.kind_of? String   # the filename has changed 
+      if @par.kind_of? String   # the filename has changed 
         # first copy the original file to new location (or template if it is a new file)
-        FileUtils.cp(@filename || File.dirname(__FILE__)+'/empty_file_template.ods', new_filename_or_io_object)
-        @filename = new_filename_or_io_object
+        FileUtils.cp(@filename || File.dirname(__FILE__)+'/empty_file_template.ods', @par)
+        @filename = @par
       end
       Zip::File.open(@filename) do |zip|
         # it is easy, because @xmlnode in in sync with contents all the time
--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -11,29 +11,42 @@ describe Rspreadsheet do
     spreadsheet.save(tmp_filename)                                  # and save spreadsheet as temp file
     
     # now compare content saved file to original
-    contents_of_files_are_identical($test_filename,tmp_filename)
+    contents_of_files_are_identical?($test_filename,tmp_filename).should == true
   end
   
-#   it 'can open spreadsheet and store it to IO object', :xpending => 'Under development' do
-#     spreadsheet = Rspreadsheet.new($test_filename)                 # open a file
-#     
-#     stringio = StringIO.new
-#     spreadsheet.save(stringio)
-#     raise stringio.read
-#   
-#   end
+  it 'can open spreadsheet and store it to IO object', :pending => 'Under development' do
+    spreadsheet = Rspreadsheet.new($test_filename)                 # open a file
+    
+    stringio = StringIO.new
+    spreadsheet.save(stringio)
+    
+    stringio.size.should > 30000
+    
+    # save it to temp file
+    tmp_filename = '/tmp/testfile1.ods'        
+    File.delete(tmp_filename) if File.exists?(tmp_filename)         # first delete temp file
+    # and save stream as temp file
+    File.open(tmp_filename, "w") do |f|
+      f.write stringio.read                                  
+    end
+     
+    contents_of_files_are_identical?($test_filename,tmp_filename).should == true
+  end
 end
 
-def contents_of_files_are_identical(filename1,filename2)
+def contents_of_files_are_identical?(filename1,filename2)
   @content_xml1 = Zip::File.open(filename1) do |zip|
     LibXML::XML::Document.io zip.get_input_stream('content.xml')
   end
   @content_xml2 = Zip::File.open(filename2) do |zip|
     LibXML::XML::Document.io zip.get_input_stream('content.xml')
   end
+  return contents_are_identical?(@content_xml1,@content_xml2)
+end
+
+def contents_are_identical?(content_xml1,content_xml2)
+  content_xml2.root.first_diff(content_xml1.root).should be_nil
+  content_xml1.root.first_diff(content_xml2.root).should be_nil
   
-  @content_xml2.root.first_diff(@content_xml1.root).should be_nil
-  @content_xml1.root.first_diff(@content_xml2.root).should be_nil
-  
-  (@content_xml1.root == @content_xml2.root).should == true
+  return (content_xml1.root == content_xml2.root)
 end
