3d537e6f752aca9df34b0a9878fe48dfc505a559
Jakub Těšínský <jAkub.cz>
2017-03-25 00:51:22 +0100

    moving datetime test to feature mranch only

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -276,7 +276,7 @@ describe Rspreadsheet::Cell do
     @cell.value.min.should eq 42
     @cell.value.sec.should eq 0
   end
-  it 'can read various types of times', :xpending => 'see is' do
+  it 'can read various types of times', :pending => 'this is tested in feature-datetimebranch' do
 #     raise @sheet2.cell('D23').xml.inspect
     expect {@cell = @sheet2.cell('D22'); @cell.value }.not_to raise_error
     expect {@cell = @sheet2.cell('D23'); @cell.value }.not_to raise_error
