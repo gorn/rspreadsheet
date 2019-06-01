d8b724d0bf2cc0df5757ecd8a6bab2ad0fdeb6f0
Jakub Těšínský <j@kub.cz>
2019-06-01 13:38:53 +0200

    correcting XMLnode comparison and test

--- lib/helpers/class_extensions.rb
+++ lib/helpers/class_extensions.rb
@@ -72,7 +72,7 @@ else # Monkeypatching
   end
 
   class LibXML::XML::Node
-    def ==(node2)  
+    def ===(node2)  
       self.simplification_of?(node2) and node2.simplification_of?(self)
     end
     # if node2 contains at least all that I do
@@ -88,15 +88,21 @@ else # Monkeypatching
       self.attributes.each do |attr|
         return "#{where}> Attribute #{attr} have diffent values: #{attr.value} != #{node2.attributes[attr.name]}" unless node2.attributes[attr.name] == attr.value
       end
+
+      elems1 = self.children
+      elems2 = node2.children
+      return "#{where}> elements have different number of children #{elems1.length} !=  #{elems2.length}" if (elems1.length != elems2.length)
       
-      elems1 = self.elements
-      elems2 = node2.elements
-  #     return "#{where}> elements have different number of subelements #{elems1.length} !=  #{elems2.length}" if (elems1.length != elems2.length) 
-      elems1.length.times do |i|
-        if (elems1[i].node_type_name == 'text') && ((elems1[i].to_s != elems2[i].to_s) )
-          return  "#{where}> #{i+1}th text subelements are different: #{elems1[i].to_s} != #{elems2[i].to_s}"
-        elsif (elems1[i].node_type_name == 'element') && (!elems1[i].simplification_of?(elems2[i]))
-          return "#{where}/[#{i+1}]#{elems1[i].first_diff(elems2[i])}"
+      elems1.each_index do |i|
+        raise "Nil for i=#{i}" if elems1[i].nil?
+        if (elems1[i].node_type_name == 'text') 
+          if elems2[i].nil? || (elems1[i].to_s) != (elems2[i].to_s)
+            return  "#{where}> #{i+1}th text children are different: #{elems1[i].to_s} != #{elems2[i].to_s}"
+          end
+        elsif (elems1[i].node_type_name == 'element') 
+          unless elems1[i].simplification_of?(elems2[i])
+            return "#{where}/[#{i+1}]#{elems1[i].first_diff(elems2[i])}"
+          end
         end
       end
         
